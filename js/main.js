5757a59441091b8a93a943dec2507d7444a17fae
Sammi <s13thapa@gmail.com>
2018-04-17 02:51:32 +0100

    Some updates

--- js/main.js
+++ js/main.js
@@ -4,7 +4,16 @@ $(window).scroll(function() {
 
 function parallax() {
 	var wScroll = $(window).scrollTop();
+	// var wScroll = -200;
 	
 	$('.parallax-bg').css('background-position', 'center '
 		+(wScroll*0.75)+'px')
 }
+
+$(window).scroll(function(){
+	if($(document).scrollTop()>50){
+		$('nav').addClass('shrink');
+	} else {
+		$('nav').removeClass('shrink');
+	}
+})
